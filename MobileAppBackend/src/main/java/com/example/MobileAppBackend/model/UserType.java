c6d01cf0fc8a880c14d256f5c9a0f24b85366b27
ZixHD <havos25@gmail.com>
2025-12-29 17:14:32 +0100

    Moved Backend to another repository

--- MobileAppBackend/src/main/java/com/example/MobileAppBackend/model/UserType.java
+++
@@ -1,6 +0,0 @@
-package com.example.MobileAppBackend.model;
-
-public enum UserType {
-
-    USER, CLIENT
-}
--- MobileAppBackend/src/main/java/com/example/MobileAppBackend/repository/TagRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.MobileAppBackend.repository;
-
-import com.example.MobileAppBackend.model.Tag;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TagRepository extends MongoRepository<Tag, String> {
-
-    boolean existsTagByName(String name);
-}
